9f16ca54637e1251b50211016ebc49ced83b6a42
Kissan <173943794+KissanM1@users.noreply.github.com>
2024-07-31 13:56:39 -0400

    Adds fucntionality to buttons

--- script.js
+++ script.js
@@ -2,6 +2,52 @@ let num1 = null;
 let num2 = null;
 let operator = null;
 
+const display = document.querySelector(".display");
+
+//Assigns html number button elements to variables
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const ten = document.querySelector(".ten");
+const zero = document.querySelector(".zero");
+
+//Assigns html operation button elements to variables
+const multiplyButton = document.querySelector(".multiplyButton");
+const divideButton = document.querySelector(".divideButton");
+const addButton = document.querySelector(".addButton");
+const subtractButton = document.querySelector(".subtractButton");
+const equalsButton = document.querySelector(".equalsButton");
+const clearButton = document.querySelector(".clearButton");
+
+
+//Updates display with button corresponding symbol
+one.addEventListener("click", () => updateDisplay(1, operator));
+two.addEventListener("click", () => updateDisplay(2, operator));
+three.addEventListener("click", () => updateDisplay(3, operator));
+four.addEventListener("click", () => updateDisplay(4, operator));
+five.addEventListener("click", () => updateDisplay(5, operator));
+six.addEventListener("click", () => updateDisplay(6, operator));
+seven.addEventListener("click", () => updateDisplay(7, operator));
+eight.addEventListener("click", () => updateDisplay(8, operator));
+nine.addEventListener("click", () => updateDisplay(9, operator));
+zero.addEventListener("click", () => updateDisplay(0, operator));
+
+multiplyButton.addEventListener("click", () => updateDisplay(null, "X"));
+divideButton.addEventListener("click", () => updateDisplay(null, "/"));
+addButton.addEventListener("click", () => updateDisplay(null, "+"));
+subtractButton.addEventListener("click", () => updateDisplay(null, "-"));
+equalsButton.addEventListener("click", () => updateDisplay(null, "="));
+
+clearButton.addEventListener("click", clearDisplay);
+
+
+//Operation function
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -19,7 +65,33 @@ function divide(num1, num2){
     // Rounding error fix
 }
 
+
+// Calculation and display functions
 function operate(num1, num2, operator){
     return operator(num1, num2);
 }
 
+function clearDisplay() {
+    display.textContent = "";
+}
+
+
+function updateDisplay(number, operatorInput){
+    if (operator == null) {
+        if (num1 === null) {
+            num1 = number.toString();
+        } else if (number != null){
+            num1 += number.toString();
+        } else {
+            operator = operatorInput;
+        }
+        } else  {
+        if (num2 === null) {
+            num2 = number.toString();
+        } else {
+            num2 += number.toString();
+        }
+    }
+
+    display.textContent = `${num1} ${operator} ${num2}`;
+}
